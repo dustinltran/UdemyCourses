d7d9441ec6034e1266f960fb800a0fee4c76d81b
Dustin <dustin_tran@live.com>
2017-10-22 00:56:46 -0700

    added weather reducer

--- ModernReactwithRedux/WeatherForecaster/src/actions/index.js
+++ ModernReactwithRedux/WeatherForecaster/src/actions/index.js
@@ -1,5 +1,7 @@
+import axios from 'axios';
+
 const API_KEY = 'af88bb2a21bfd7f562bbac99ba1a2f16';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
