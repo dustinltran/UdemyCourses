eeb1e4e349c0eab35e921c29b0158a906ab4d218
Dustin <dustin_tran@live.com>
2017-10-24 01:15:19 -0700

    Fields Rendering

--- ModernReactwithRedux/Blogs/src/components/posts_new.js
+++ ModernReactwithRedux/Blogs/src/components/posts_new.js
@@ -2,11 +2,32 @@ import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
+	renderTitleField(){
+		return (
+			<div className="form-group">
+				<label>Title</label>
+				<input 
+					className="form-control"
+					type="text"
+					{...Field.input}
+				/>
+			</div>
+		);
+	}
+
+
 	render(){
 		return (
-			<div>
-				
-			</div>
+			<form>
+				<Field
+					name="title"
+					component={this.renderTitleField}
+				/>
+				<Field
+					name="tags"
+					component={this.renderTagsField}
+				/>
+			</form>
 		);
 	}
 }
