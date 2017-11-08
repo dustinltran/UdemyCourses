1145a1e13b9709bd128e2aa205ad8869dd0a4483
Dustin <dustin_tran@live.com>
2017-11-08 15:08:28 -0800

    crud complete

--- DeveloperGuidetoMongoDB/Assignment1/test/update_test.js
+++ DeveloperGuidetoMongoDB/Assignment1/test/update_test.js
@@ -8,16 +8,45 @@ describe('Updating Records', () => {
 			.then(() => done());
 	});
 
-	it('instance type using set n save', (done) => {
-		joe.set('name', 'Alex');
-		joe.save()
+	function assertName(operation, done) {
+		operation
 			.then(() => User.find({}))
 			.then((users) => {
 				assert(users.length === 1);
 				assert(users[0].name === 'Alex');
 				done();
 			});
+	}
 
+	it('instance type using set n save', (done) => {
+		joe.set('name', 'Alex');
+		assertName(joe.save(), done);
+
+	});
+
+	it('model instance can update', (done) => {
+		assertName(joe.update({name: 'Alex'}), done);
+	});
+
+	it('model class can update', (done) => {
+		assertName(
+			User.update({ name: 'Joe' }, {name: 'Alex'}),
+			done
+		);
+	})
+
+	it('model class can update one record', (done) => {
+		assertName(
+			User.findOneAndUpdate({ name: 'Joe'}, {name: 'Alex'}),
+			done
+		);
+	});
+
+	it('model class can find a record with an Id and update', (done) => {
+		assertName(
+			User.findByIdAndUpdate(joe._id, {name: 'Alex'}),
+			done
+		);
 	});
 });
 
