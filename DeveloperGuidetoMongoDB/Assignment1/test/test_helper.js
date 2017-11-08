4437306653a285a29fb743b62462d89371742b15
Dustin <dustin_tran@live.com>
2017-11-08 10:10:55 -0800

    prepping for nodemon

--- DeveloperGuidetoMongoDB/Assignment1/test/test_helper.js
+++ DeveloperGuidetoMongoDB/Assignment1/test/test_helper.js
@@ -2,11 +2,11 @@ const mongoose = require ('mongoose');
 
 mongoose.Promise = global.Promise;
 
-before(() => {
-	
+before((done) => {
+
 	mongoose.connect('mongodb://localhost/users_test');
 	mongoose.connection
-		.once('open', () => console.log('Good to go!'))
+		.once('open', () => {done();})
 		.on('error', (error) => {
 			console.warn('Warning', error);
 		});
