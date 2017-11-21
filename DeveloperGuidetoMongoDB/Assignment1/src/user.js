3e7a7621c651dc37cd4fb3b26974f34fb219199f
Dustin <dustin_tran@live.com>
2017-11-20 21:22:38 -0800

    validation

--- DeveloperGuidetoMongoDB/Assignment1/src/user.js
+++ DeveloperGuidetoMongoDB/Assignment1/src/user.js
@@ -4,8 +4,13 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	name: {
 		type: String,
+		validate: {
+			validator: () => name.length > 2,
+			message: 'Name must be longer than 2 characters'
+		}
 		required: [true, 'Name is required.']
 	},
+
 
 	postCount: Number
 
