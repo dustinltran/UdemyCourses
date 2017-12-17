50e4a925e8ea42e71063965081a79d6509fec6b7
Dustin <dustin_tran@live.com>
2017-12-17 10:06:07 -0800

    assignment 3

---
+++ ProgrammingBitcoin/Assignment2/get_stats.py
@@ -0,0 +1,10 @@
+from blockchain import statistics
+
+stats = statistics.get()
+
+print("Bitcoin Trade Volume: %s" % stats.trade_volume_btc)
+
+print("Bitcoin mined %s\n" % stats.btc_mined)
+
+print(stats.market_price_usd)
+
